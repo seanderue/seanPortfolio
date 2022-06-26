474353166c4943754365061604769c49866090c7
Sean <Sean@MacBook-Pro-2.local>
2022-06-25 21:51:41 -0400

    updated text

--- pages/project/tenzies.jsx
+++ pages/project/tenzies.jsx
@@ -58,12 +58,12 @@ export default function Tenzies() {
                 <li>Fun confetti upon winning 😉</li>
               </ul>
               <h4 className={styles.subtitle}>Outcome</h4>
-              <p className={styles.body_text}>I made a playable game! The back-end is server-validated. So Player A make an input, the server takes the data, runs it through the game logic, and updates the client-side state of Players A, B, C, and D in the target game-room.
-                <br/><br/>
-                I definitely learned how unglamorous backend can be, as I put in a lot of work for someone to click a dice and have it turn green on multiple clients. That said, I have the barebones framework complete, and it would be relatively easy to add more features like player power-ups/weapons that they could interact with the enemy board with.
-                <br/><br/>
-                Eventually I’d like to go back and polish the CSS and add those features. But for a project that was meant to learn how to build my own back-end &amp; real-time database, (while working on my React muscles), I checked my boxes. <br/><br/>At the time, I had to get rolling on more projects so I could be that much closer to employment.</p>
-
+              <p className={styles.body_text}>I made a playable game! The back-end is server-validated. So Player A makes an input, the server takes the data, runs it through the game logic, and updates the client-side state of Players A, B, C, and D in the room.
+<br/><br/>
+I definitely learned how unglamorous backend can be. I put in a lot of work for someone to click a dice and have it turn green on multiple clients. That said, I have the barebones framework complete, and it would be relatively easy to add more features like player power-ups/weapons that they could interact with the enemy board.
+<br/><br/>
+Eventually, I’d like to go back and polish the CSS and add those features. But I checked my boxes on this project. I learned how to build my own back-end &amp; real-time database and I worked out my React muscles. At the time, I had to get rolling on more projects so I could be that much closer to employment.
+</p>
 
           </div>
         </div>
